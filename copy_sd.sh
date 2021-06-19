c29431e51576a8a39a6e9b38f8b64fa045c362fa
guanglun <272073020@qq.com>
2021-06-19 08:38:53 -0700

    update

---
+++ copy_sd.sh
@@ -0,0 +1,4 @@
+sudo cp images/linux/image.ub /media/guanglun/BOOT/
+sudo cp images/linux/BOOT.BIN /media/guanglun/BOOT/
+sudo cp images/linux/boot.scr /media/guanglun/BOOT/
+sudo tar xzvf images/linux/rootfs.tar.gz -C /media/guanglun/rootfs
